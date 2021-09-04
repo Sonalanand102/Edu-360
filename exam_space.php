0432b0a70eefddd8a738df82ab6a9a2006a647c7
Nitish Kumar <krnitish1207@gmail.com>
2021-09-05 01:35:42 +0530

    created exam_space

--- exam_space.php
+++ exam_space.php
@@ -1,1 +1,244 @@
 
+<?php
+    include 'assets/header.php';
+    include 'assets/navbar.php';
+?>
+
+<div class="container" style='margin-top:150px; margin-bottom:32px;'>
+<div id="accordion">
+  <div class="card" >
+    <div class="card-header" id="headingOne" style='background-color:#121e42;'>
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          COMPUTER SCIENCE AND ENGINEERING 
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show " aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body ">
+
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active  ">
+      
+    <div class="subject-cards d-flex justify-content-center">
+
+    <!-- Single Blog Start -->
+    <div class="blog-items mb-30 mb-xs-10 col-md-3 col-sm-3"
+                    onmouseover="this.style.transform='scale(1.03)'" onmouseout="this.style.transform='scale(1)'">
+                    <div class="blog-inner heading-color">
+                        <div class="blog-image">
+                            <a href="" class="image"><img
+                                    src="assets\images\dsaa.jpg" alt="" height='100%' width='100%'></a>
+                            <ul class="meta theme-color" style="list-style: none;">
+                                <li>4-Sept-2021<i class="fa fa-clock"></i></li>
+                            </ul>
+                        </div>
+                        <div class="content" style="margin-left: 20px;">
+                            <h3 class="title"><a
+                                    href="">Data Structure and Algorithms</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Blog End -->
+
+        
+
+        <!-- Single Blog Start -->
+        <div class="blog-items mb-30 mb-xs-10 col-md-3 col-sm-3"
+                    onmouseover="this.style.transform='scale(1.03)'" onmouseout="this.style.transform='scale(1)'">
+                    <div class="blog-inner heading-color">
+                        <div class="blog-image">
+                            <a href="" class="image"><img
+                                    src="assets\images\daaa.jpg" alt="" height='100%' width='100%'></a>
+                            <ul class="meta theme-color" style="list-style: none;">
+                                <li>4-Sept-2021<i class="fa fa-clock"></i></li>
+                            </ul>
+                        </div>
+                        <div class="content" style="margin-left: 20px;">
+                            <h3 class="title"><a
+                                    href="">Design and Analysis of Algorithms</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Blog End -->
+
+
+                <!-- Single Blog Start -->
+        <div class="blog-items mb-30 mb-xs-10 col-md-3 col-sm-3"
+                    onmouseover="this.style.transform='scale(1.03)'" onmouseout="this.style.transform='scale(1)'">
+                    <div class="blog-inner heading-color">
+                        <div class="blog-image">
+                            <a href="" class="image"><img
+                                    src="assets\images\oss.jpg" alt="" height='100%' width='100%'></a>
+                            <ul class="meta theme-color" style="list-style: none;">
+                                <li>4-Sept-2021<i class="fa fa-clock"></i></li>
+                            </ul>
+                        </div>
+                        <div class="content" style="margin-left: 20px;">
+                            <h3 class="title"><a
+                                    href="">Operating System</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Blog End -->
+
+    </div>
+
+        
+
+
+    </div>
+    <div class="carousel-item ">
+      
+
+    <div class="subject-cards d-flex justify-content-center">
+
+<!-- Single Blog Start -->
+<div class="blog-items mb-30 mb-xs-10 col-md-3 col-sm-3"
+                onmouseover="this.style.transform='scale(1.03)'" onmouseout="this.style.transform='scale(1)'">
+                <div class="blog-inner heading-color">
+                    <div class="blog-image">
+                        <a href="" class="image"><img
+                                src="assets\images\coaa.jpg" alt="" height='100%' width='100%'></a>
+                        <ul class="meta theme-color" style="list-style: none;">
+                            <li>4-Sept-2021<i class="fa fa-clock"></i></li>
+                        </ul>
+                    </div>
+                    <div class="content" style="margin-left: 20px;">
+                        <h3 class="title"><a
+                                href="">Computer Organization and Architecture</a>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Blog End -->
+
+    
+
+    <!-- Single Blog Start -->
+    <div class="blog-items mb-30 mb-xs-10 col-md-3 col-sm-3"
+                onmouseover="this.style.transform='scale(1.03)'" onmouseout="this.style.transform='scale(1)'">
+                <div class="blog-inner heading-color">
+                    <div class="blog-image">
+                        <a href="" class="image"><img
+                                src="assets\images\dbmss.jpg" alt="" height='100%' width='100%'></a>
+                        <ul class="meta theme-color" style="list-style: none;">
+                            <li>4-Sept-2021<i class="fa fa-clock"></i></li>
+                        </ul>
+                    </div>
+                    <div class="content" style="margin-left: 20px;">
+                        <h3 class="title"><a
+                                href="">Database Manangement System</a>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Blog End -->
+
+
+            <!-- Single Blog Start -->
+    <div class="blog-items mb-30 mb-xs-10 col-md-3 col-sm-3"
+                onmouseover="this.style.transform='scale(1.03)'" onmouseout="this.style.transform='scale(1)'">
+                <div class="blog-inner heading-color">
+                    <div class="blog-image">
+                        <a href="" class="image"><img
+                                src="assets\images\tocc.jpg" alt="" height='100%' width='100%'></a>
+                        <ul class="meta theme-color" style="list-style: none;">
+                            <li>4-Sept-2021<i class="fa fa-clock"></i></li>
+                        </ul>
+                    </div>
+                    <div class="content" style="margin-left: 20px;">
+                        <h3 class="title"><a
+                                href="">Theory of Computation</a>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <!-- Single Blog End -->
+
+</div>
+
+        
+
+
+               
+
+    </div>
+
+
+
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+        
+       
+
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo" style='background-color:#121e42;'>
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          CIVIL ENGINEERING
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+          Similar as Computer Science and Engineering's part
+      </div>
+    </div>
+  </div>
+
+
+  <div class="card">
+    <div class="card-header" id="headingThree" style='background-color:#121e42;'>
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Electrical and Electronics Engineering
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+         Similar as Computer Science and Engineering's part
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header" id="headingThree" style='background-color:#121e42;'>
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Mechanical Engineering
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+         Similar as Computer Science and Engineering's part
+      </div>
+    </div>
+  </div>
+
+
+</div>
+</div>
+
+<?php
+    include 'assets/footer.php';
+?>
